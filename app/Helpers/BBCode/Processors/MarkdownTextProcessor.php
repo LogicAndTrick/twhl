486bd0f1b0c398f14a8cc59607b667c7522fd2a6
Daniel Walder <daniel.walder@gmail.com>
2016-02-29 21:38:15 +1000

    Change the behaviour of the markdown text processor so it follows better whitespace boundary rules

--- app/Helpers/BBCode/Processors/MarkdownTextProcessor.php
+++ app/Helpers/BBCode/Processors/MarkdownTextProcessor.php
@@ -16,20 +16,24 @@ class MarkdownTextProcessor extends Processor {
          * Very simple rules: no nesting, no newlines, must start/end on a word boundary
          */
 
+        $pre = '%(?<=^|[\p{P}\s])';
+        $mid = '([^<>\r\n]*?)';
+        $post = '(?=[\p{P}\s]|$)%imu';
+
         // Bold
-        $text = preg_replace('/\*([.,;\'"]*?\b[^<>\r\n]*?\b[.,;\'"]*?)\*/si', '<strong>$1</strong>', $text);
+        $text = preg_replace("{$pre}\*{$mid}\*{$post}", '<strong>$1</strong>', $text);
 
         // Italics
-        $text = preg_replace('%/([.,;\'"]*?\b[^<>\r\n]*?\b[.,;\'"]*?)/%si', '<em>$1</em>', $text);
+        $text = preg_replace("{$pre}/{$mid}/{$post}", '<em>$1</em>', $text);
 
         // Underline
-        $text = preg_replace('/\b_([^<>\r\n]*?)_\b/si', '<span class="underline">$1</span>', $text);
+        $text = preg_replace("{$pre}_{$mid}_{$post}", '<span class="underline">$1</span>', $text);
 
         // Strikethrough
-        $text = preg_replace('/~([.,;\'"]*?\b[^<>\r\n]*?\b[.,;\'"]*?)~/si', '<span class="strikethrough">$1</span>', $text);
+        $text = preg_replace("{$pre}-{$mid}-{$post}", '<span class="strikethrough">$1</span>', $text);
 
         // Code
-        $text = preg_replace('/`([.,;\'"]*?\b[^<>]*?\b[.,;\'"]*?)`/si', '<code>$1</code>', $text);
+        $text = preg_replace("{$pre}`{$mid}`{$post}", '<code>$1</code>', $text);
 
         return $text;
     }
