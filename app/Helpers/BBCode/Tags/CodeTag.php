ade36512277fefa064b33a89ca14757c707316f4
Daniel Walder <daniel.walder@gmail.com>
2019-04-15 18:28:41 +1000

    Fix double escape in the code tag

--- app/Helpers/BBCode/Tags/CodeTag.php
+++ app/Helpers/BBCode/Tags/CodeTag.php
@@ -12,7 +12,7 @@ class CodeTag extends Tag {
 
     public function FormatResult($result, $parser, $state, $scope, $options, $text)
     {
-        $text = $parser->CleanString($text);
+        // The text is already html escaped at this point (the default element cleans all strings)
         return '<code>' . $text . '</code>';
     }
 }
