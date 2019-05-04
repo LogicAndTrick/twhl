049c5dd80d015b20818f64d2d53d35db97bade2c
Daniel Walder <daniel.walder@gmail.com>
2019-05-04 17:18:14 +1000

    Null pointer fix on user post listing

--- resources/views/forums/post/index.blade.php
+++ resources/views/forums/post/index.blade.php
@@ -18,55 +18,62 @@
 
     <div class="post-listing">
         @foreach ($posts as $post)
+            {? $thread = $post->thread; ?}
             <div class="slot post" id="post-{{ $post->id }}">
-                <div class="slot-heading">
-                    <div class="slot-avatar hidden-md-up">
-                        @avatar($post->user small show_name=false)
+                @if ($thread != null)
+                    <div class="slot-heading">
+                        <div class="slot-avatar hidden-md-up">
+                            @avatar($post->user small show_name=false)
+                        </div>
+                        <div class="slot-title hidden-md-up">
+                            @avatar($post->user text)
+                            <div class="pull-right">
+                                @if ($post->isEditable($post->thread))
+                                    <a href="{{ act('post', 'edit', $post->id) }}" class="btn btn-xs btn-outline-primary">
+                                        <span class="fa fa-pencil"></span>
+                                        <span class="hidden-xs-down">Edit</span>
+                                    </a>
+                                @endif
+                                @if (permission('ForumAdmin'))
+                                    <a href="{{ act('post', 'delete', $post->id) }}" class="btn btn-xs btn-outline-danger">
+                                        <span class="fa fa-remove"></span>
+                                        <span class="hidden-xs-down">Delete</span>
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="slot-subtitle">
+                            Posted @date($post->created_at)
+                            <br class="hidden-md-up" />
+                            in <a href="{{ act('thread', 'view', $post->thread_id) }}">{{ $post->thread->title }}</a>
+                            <a class="pull-right" href="{{ act('thread', 'locate-post', $post->id) }}">Post #{{ $post->id }}</a>
+                        </div>
                     </div>
-                    <div class="slot-title hidden-md-up">
-                        @avatar($post->user text)
-                        <div class="pull-right">
+                    <div class="slot-row">
+                        <div class="slot-main">
+                            <div class="bbcode post-content">{!! $post->content_html !!}</div>
+                        </div>
+                        <div class="slot-right hidden-sm-down">
+                            @avatar($post->user full)
                             @if ($post->isEditable($post->thread))
                                 <a href="{{ act('post', 'edit', $post->id) }}" class="btn btn-xs btn-outline-primary">
                                     <span class="fa fa-pencil"></span>
-                                    <span class="hidden-xs-down">Edit</span>
+                                    Edit
                                 </a>
                             @endif
                             @if (permission('ForumAdmin'))
                                 <a href="{{ act('post', 'delete', $post->id) }}" class="btn btn-xs btn-outline-danger">
                                     <span class="fa fa-remove"></span>
-                                    <span class="hidden-xs-down">Delete</span>
+                                    Delete
                                 </a>
                             @endif
                         </div>
                     </div>
-                    <div class="slot-subtitle">
-                        Posted @date($post->created_at)
-                        <br class="hidden-md-up" />
-                        in <a href="{{ act('thread', 'view', $post->thread_id) }}">{{ $post->thread->title }}</a>
-                        <a class="pull-right" href="{{ act('thread', 'locate-post', $post->id) }}">Post #{{ $post->id }}</a>
+                @else
+                    <div class="text-center m-2 font-italic">
+                        This post was made on a thread that has been deleted.
                     </div>
-                </div>
-                <div class="slot-row">
-                    <div class="slot-main">
-                        <div class="bbcode post-content">{!! $post->content_html !!}</div>
-                    </div>
-                    <div class="slot-right hidden-sm-down">
-                        @avatar($post->user full)
-                        @if ($post->isEditable($post->thread))
-                            <a href="{{ act('post', 'edit', $post->id) }}" class="btn btn-xs btn-outline-primary">
-                                <span class="fa fa-pencil"></span>
-                                Edit
-                            </a>
-                        @endif
-                        @if (permission('ForumAdmin'))
-                            <a href="{{ act('post', 'delete', $post->id) }}" class="btn btn-xs btn-outline-danger">
-                                <span class="fa fa-remove"></span>
-                                Delete
-                            </a>
-                        @endif
-                    </div>
-                </div>
+                @endif
             </div>
         @endforeach
     </div>
