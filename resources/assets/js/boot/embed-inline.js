3ecc3bf9988109f2d974c974a12c93fcd17071f8
Daniel Walder <daniel.walder@gmail.com>
2019-05-13 16:48:47 +1000

    Another attempt

--- resources/assets/js/boot/embed-inline.js
+++ resources/assets/js/boot/embed-inline.js
@@ -38,7 +38,7 @@ $(function() {
                     for (var po in data.pages) {
                         var p = data.pages[po];
                         var pl = c.find('a[href*="://' + window.location.host + '/wiki/page/"]').filter(function (i, x) {
-                            var hr = x.href, uc = '://' + window.location.host + '/wiki/page/' + encodeURIComponent(p.slug);
+                            var hr = decodeURI(x.href), uc = '://' + window.location.host + '/wiki/page/' + p.slug;
                             return hr && hr.toLowerCase().indexOf(uc.toLowerCase()) >= 0;
                         });
                         if (!p.exists) pl.addClass('text-danger').attr('title', 'Page does not exist yet - click to create it');
@@ -46,7 +46,7 @@ $(function() {
                     for (var eo in data.embeds) {
                         var e = data.embeds[eo];
                         var el = c.find('a[href*="://' + window.location.host + '/wiki/embed/"]').filter(function (i, x) {
-                            var hr = x.href, uc = '://' + window.location.host + '/wiki/embed/' + encodeURIComponent(e.slug);
+                            var hr = decodeURI(x.href), uc = '://' + window.location.host + '/wiki/embed/' + e.slug;
                             return hr && hr.toLowerCase().indexOf(uc.toLowerCase()) >= 0;
                         });
                         if (!e.exists) {
