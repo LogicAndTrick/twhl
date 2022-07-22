6c2ba6d454072465c95d194cc17bd5049f45f752
Daniel Walder <daniel.walder@gmail.com>
2022-07-22 21:44:28 +1000

    Fix #63 (again)

--- app/Models/Forums/ForumThread.php
+++ app/Models/Forums/ForumThread.php
@@ -75,7 +75,10 @@ class ForumThread extends Model {
         // 4a. If the thread is sticky, it can always be posted in
         if ($this->is_sticky) return true;
 
-        // 4b. Normal threads are closed if they are over ForumThread::THREAD_LOCK_DAYS days old
+        // 4b. Empty threads can't be posted in
+        if (!$this->last_post) return false;
+
+        // 4c. Normal threads are closed if they are over ForumThread::THREAD_LOCK_DAYS days old
         if (Date::DiffDays(Date::Now(), $this->last_post->updated_at) > ForumThread::THREAD_LOCK_DAYS) return false;
 
         return true;
@@ -90,6 +93,7 @@ class ForumThread extends Model {
         if (!Auth::user()) return 'You must be logged in to post a response.';
         if (!permission('ForumCreate')) return 'You do not have permission to post a response.';
         if (!$this->is_open) return 'This thread has been closed, responses cannot be posted.';
+        if (!$this->last_post) return 'This thread has no posts, probably caused by older versions of the forum software. You cannot post in it.';
         if (Date::DiffDays(Date::Now(), $this->last_post->updated_at) > ForumThread::THREAD_LOCK_DAYS) return 'This thread has automatically been locked because it has been idle for over ' . ForumThread::THREAD_LOCK_DAYS . ' days.';
         return null;
     }
