ea828689ae105d87043473a429ee2d95267b556c
Daniel Walder <daniel.walder@gmail.com>
2023-01-02 11:51:24 +1000

    Add svg icon dimensions and example query

--- resources/views/search/opensearchdescription.blade.php
+++ resources/views/search/opensearchdescription.blade.php
@@ -6,9 +6,11 @@
   <Description>The Whole Half-Life - Search forum threads, wiki articles, maps, and more</Description>
   <Tags>GoldSource GoldSrc Half-Life Hammer Mapping Maps Modding Mods Source Tutorials TWHL Valve</Tags>
   
-  <Image type="image/svg+xml">{{ asset('images/twhl-logo.svg') }}</Image>
+  <Image width="64" height="64" type="image/svg+xml">{{ asset('images/twhl-logo.svg') }}</Image>
   <Image width="64" height="64" type="image/png">{{ asset('images/logo_icon_64.png') }}</Image>
 
   <Url type="text/html" template="{{ act('search', 'index') }}?search={searchTerms}" />
+  <Url type="application/opensearchdescription+xml" rel="self" template="{{ url('/opensearch.xml') }}" />
+  <Query role="example" searchTerms="test" />
   <moz:SearchForm>{{ act('search', 'index') }}</moz:SearchForm>
 </OpenSearchDescription>
