b3a99ec44e1c31658b0f8d1635071fd43fa53b1d
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-02-08 19:25:26 +0100

    Code cleanup in form-validation.js

--- resources/assets/js/boot/form-validation.js
+++ resources/assets/js/boot/form-validation.js
@@ -3,24 +3,20 @@ $(() => {
         element.addEventListener("invalid", ({ target }) => {
             const patternDescription = target.dataset?.patterndescription;
             const { customError, patternMismatch } = target.validity ?? {};
-            if (customError || !patternMismatch || !patternDescription) {
-                return;
+            if (!customError && patternMismatch && patternDescription) {
+                target.setCustomValidity(patternDescription);
+                target.reportValidity();
             }
-
-            target.setCustomValidity(patternDescription);
-            target.reportValidity();
         });
 
         element.addEventListener("input", ({ target }) => {
             const patternDescription = target.dataset?.patterndescription;
             const { customError, patternMismatch } = target.validity ?? {};
 
-            if (!customError || patternMismatch || !patternDescription) {
-                return;
+            if (customError && !patternMismatch && patternDescription) {
+                target.setCustomValidity('');
+                target.reportValidity();
             }
-
-            target.setCustomValidity('');
-            target.reportValidity();
         });
     }
 });
